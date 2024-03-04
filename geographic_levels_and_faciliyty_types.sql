14ca3a2b521738d3774cb40308d9a7eed17a1d25
kevel <keveltiah@gmail.com>
2024-03-04 12:44:12 +0100

    Ajustement

---
+++ geographic_levels_and_faciliyty_types.sql
@@ -0,0 +1,11 @@
+insert into referencedata.geographic_levels (id, code, levelnumber) values ('6b78e6c6-292e-4733-bb9c-3d802ad61206', 'Country', 1);
+insert into referencedata.geographic_levels (id, code, levelnumber) values ('9b497d87-cdd9-400e-bb04-fae0bf6a9491', 'Region', 2);
+insert into referencedata.geographic_levels (id, code, levelnumber) values ('93c05138-4550-4461-9e8a-79d5f050c223', 'District', 3);
+insert into referencedata.geographic_levels (id, code, levelnumber) values ('90e35999-a64f-4312-ba8f-bc13a1311c75', 'Municipality', 4);
+insert into referencedata.geographic_levels (id, code, levelnumber) values ('89e35999-a64f-4312-ba8f-bc13a1311f21', 'Village', 5);
+
+INSERT INTO referencedata.facility_types (id, active, code, name) VALUES(
+'3b8f731e-9aa2-4f29-af8c-20d0b7f40999', true, 'C', 'Health  center'
+),
+('3b8f732e-9aa2-4f29-af8c-20d0b7f40999', true, 'D', 'Dispensary'),
+('3b8f731e-9aa2-4f29-af8c-20d0b7f40992', true, 'H', 'Hospital');
