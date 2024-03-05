804fcea368f48611b0c64cce276eea740050dec6
kevel <keveltiah@gmail.com>
2024-03-05 17:14:24 +0100

    types de produits

---
+++ referencedata.orderable_disp.sql
@@ -0,0 +1,3 @@
+insert into referencedata.orderable_display_categories (id, code, displayname,displayorder) values (
+'9254a349-8d6a-4a52-9e9d-a86b40552160', '001', 'Comprimé', 1),
+('04491ab2-422a-4d52-9070-a163db597976', '002', 'Vaccin', 2);
