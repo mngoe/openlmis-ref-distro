2b4897eca6e79a18086dc28dfce5c2e00713b2df
kevel <keveltiah@gmail.com>
2024-03-08 12:31:20 +0100

    Ajout autres donnée d'intégration fagep

---
+++ autres_donnee_fagep.sql
@@ -0,0 +1,64 @@
+
+INSERT INTO referencedata.supervisory_nodes (id, code, name, facilityid, parentid) VALUES (
+'6cf75e80-6405-458c-b49f-001e81411c94', 'FR1', 'Point d''approbation centrale commandes programmes palu FRPS Adamaoua', 'c3c0263f-b671-4e5e-999d-f02ed290cd7b', null
+),
+('7cf75e10-6405-458c-b49f-001e81411c89', 'FR1.1', 'Sous-point d''approbation commandes programmes palu FRPS Adamaoua', 'c3c0263f-b671-4e5e-999d-f02ed290cd7b', '6cf75e80-6405-458c-b49f-001e81411c94'),
+('8cf76e80-6405-458c-b49f-001e81411c77', 'FR1.2', 'Sous-point d''approbation commandes programmes palu FRPS Adamaoua 2', 'c3c0263f-b671-4e5e-999d-f02ed290cd7b', '6cf75e80-6405-458c-b49f-001e81411c94'),
+
+('9cf76e20-6405-458c-b49f-001e81411c74', 'FR2', 'Point d''approbation centrale commandes programmes palu FRPS Centre', 'b00b1291-1da2-4807-bbe1-381c0ef61322', null),
+('1cf76e30-6405-458c-b49f-001e81411c82', 'FR2.1', 'Sous-point d''approbation centrale commandes programmes palu FRPS Centre', 'b00b1291-1da2-4807-bbe1-381c0ef61322', '9cf76e20-6405-458c-b49f-001e81411c74'),
+('2cf76e30-6405-458c-b49f-001e81411c36', 'FR2.2', 'Sous-point d''approbation centrale commandes programmes palu FRPS Centre 2', 'b00b1291-1da2-4807-bbe1-381c0ef61322', '9cf76e20-6405-458c-b49f-001e81411c74'),
+
+('8cf76e33-6405-458c-b49f-001e81478c70', 'FOSA1', 'Point d''approbation centrale commandes programmes palu Ako District Hospital', '864257ce-d200-44b7-8b65-3b286c6931df', null),
+('3cf76e10-6405-458c-b41f-001e81411c72', 'FOSA1.1', 'Sous-point d''approbation centrale commandes programmes palu Ako District Hospital', '864257ce-d200-44b7-8b65-3b286c6931df', '8cf76e33-6405-458c-b49f-001e81478c70'),
+('5cf76e37-6405-458c-b47f-001e81411c22', 'FOSA1.2', 'Sous-point d''approbation centrale commandes programmes palu Ako District Hospital 2', '864257ce-d200-44b7-8b65-3b286c6931df', '8cf76e33-6405-458c-b49f-001e81478c70'),
+
+('8cf76e08-6405-778c-b49f-001e81478c04', 'FOSA2', 'Point d''approbation centrale commandes programmes palu Akwaya District Hospital', '97b42fd8-b0a1-41fd-8f75-b576203485a1', null),
+('3cf76e10-6405-458c-b41f-001e81411c09', 'FOSA2.1', 'Sous-point d''approbation centrale commandes programmes palu Akwaya District Hospital', '97b42fd8-b0a1-41fd-8f75-b576203485a1', '8cf76e08-6405-778c-b49f-001e81478c04'),
+('5cf76e07-6405-458c-b47f-001e81411c23', 'FOSA2.2', 'Sous-point d''approbation centrale commandes programmes palu Akwaya District Hospital 2', '97b42fd8-b0a1-41fd-8f75-b576203485a1', '8cf76e08-6405-778c-b49f-001e81478c04'),
+
+('8af76e08-6405-778c-b49f-001e81478c02', 'FOSA3', 'Point d''approbation centrale commandes programmes palu Azire Integrated Health Centre', '19f99865-defa-4161-9389-7ef231bcd626', null),
+('3ef76e10-6405-458c-b42f-001e81411c01', 'FOSA3.1', 'Sous-point d''approbation centrale commandes programmes palu Azire Integrated Health Centre', '19f99865-defa-4161-9389-7ef231bcd626', '8af76e08-6405-778c-b49f-001e81478c02'),
+('5cf76e97-6405-458c-b47f-002e81411c20', 'FOSA3.2', 'Sous-point d''approbation centrale commandes programmes palu Azire Integrated Health Centre 2', '19f99865-defa-4161-9389-7ef231bcd626', '8af76e08-6405-778c-b49f-001e81478c02'),
+
+('7bf76e02-6405-778c-b49f-001e81222c09', 'FOSA4', 'Point d''approbation centrale commandes programmes palu Bafmeng Medicalised Health centre', 'bfd37f57-29a5-4e40-87b2-874d228581c4', null),
+('6bf76e32-6404-778c-b49f-001e82145c05', 'FOSA4.1', 'Sous-point d''approbation centrale commandes programmes palu Bafmeng Medicalised Health centre', 'bfd37f57-29a5-4e40-87b2-874d228581c4', '7bf76e02-6405-778c-b49f-001e81222c09'),
+('6bf76e06-6405-778c-b49f-001e82544c08', 'FOSA4.2', 'Sous-point d''approbation centrale commandes programmes palu Bafmeng Medicalised Health centre 2', 'bfd37f57-29a5-4e40-87b2-874d228581c4', '7bf76e02-6405-778c-b49f-001e81222c09'),
+
+('7df72e02-7775-778c-b49f-001e81400d77', 'CE1', 'Point d''approbation centrale commandes programmes palu CENAME', '76bfd0ec-55c2-4d1b-87c9-97e978b3c6b5', null),
+('5df72e55-7525-778c-b49f-001e81470d11', 'CE1.1', 'Sous-point d''approbation centrale commandes programmes palu CENAME', '76bfd0ec-55c2-4d1b-87c9-97e978b3c6b5', '7df72e02-7775-778c-b49f-001e81400d77'),
+('7df72e02-6365-778c-b49f-001e81451d58', 'CE1.2', 'Sous-point d''approbation centrale commandes programmes palu CENAME 2', '76bfd0ec-55c2-4d1b-87c9-97e978b3c6b5', '7df72e02-7775-778c-b49f-001e81400d77');
+
+INSERT INTO referencedata.requisition_groups (id, code, description, name, supervisorynodeid) VALUES (
+'e5077535-4cef-45ae-a01c-99c1be8829fa', 'GRP1A', '', 'commandes produits programme palu FRPS Adamaoua', '6cf75e80-6405-458c-b49f-001e81411c94'
+),
+('e4217412-4cef-45ae-a01c-52c1be8829fb', 'GRP1B', '', 'commandes produits programme palu FRPS Adamaoua', '6cf75e80-6405-458c-b49f-001e81411c94'),
+('e4867144-4cef-45ae-a01c-52c1be1200fa', 'GRP1C', '', 'commandes produits programme palu FRPS Adamaoua', '6cf75e80-6405-458c-b49f-001e81411c94'),
+
+('dc0c57d8-2930-4ffe-951a-16aee0353e93', 'GRP2A', '', 'commandes produits programme palu FRPS Centre', '9cf76e20-6405-458c-b49f-001e81411c74'),
+('ef0c50d2-5846-4ffe-254a-16aff0243e77', 'GRP2B', '', 'commandes produits programme palu FRPS Centre', '9cf76e20-6405-458c-b49f-001e81411c74'),
+('ac7c50d0-8547-4ffe-877a-16afe5243a66', 'GRP2C', '', 'commandes produits programme palu FRPS Centre', '9cf76e20-6405-458c-b49f-001e81411c74');
+
+INSERT INTO referencedata.requisition_group_program_schedules(id, directdelivery, processingscheduleid, programid, requisitiongroupid) VALUES (
+'b0cac3da-3b65-4d3b-a1ec-e438a6774bad', true, '569dc468-07fc-4b7b-983c-dd243cd650b2', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', 'e5077535-4cef-45ae-a01c-99c1be8829fa'
+),
+('8c399a65-9e19-4f89-af6e-bcc43c32207b', true, '569dc468-07fc-4b7b-983c-dd243cd650b2', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', 'e4217412-4cef-45ae-a01c-52c1be8829fb'),
+('1c390a22-6e11-4f23-af6e-bcc43c89774e', true, '569dc468-07fc-4b7b-983c-dd243cd650b2', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', 'e4867144-4cef-45ae-a01c-52c1be1200fa'),
+('47449ae0-e114-4d3d-aaff-7a17badd55b1', true, '569dc468-07fc-4b7b-983c-dd243cd650b2', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', 'dc0c57d8-2930-4ffe-951a-16aee0353e93'),
+('96555ae0-e114-4d3d-aaff-7a33bade41b0', true, '569dc468-07fc-4b7b-983c-dd243cd650b2', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', 'ef0c50d2-5846-4ffe-254a-16aff0243e77'),
+('08066b68-8dd1-48bc-bdc7-a91217811b36', true, '569dc468-07fc-4b7b-983c-dd243cd650b2', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', 'ac7c50d0-8547-4ffe-877a-16afe5243a66');
+
+INSERT INTO referencedata.requisition_group_members (requisitiongroupid, facilityid) VALUES (
+'e5077535-4cef-45ae-a01c-99c1be8829fa', 'c3c0263f-b671-4e5e-999d-f02ed290cd7b'
+),
+('e4217412-4cef-45ae-a01c-52c1be8829fb', '864257ce-d200-44b7-8b65-3b286c6931df'),
+('e4867144-4cef-45ae-a01c-52c1be1200fa', '97b42fd8-b0a1-41fd-8f75-b576203485a1'),
+('dc0c57d8-2930-4ffe-951a-16aee0353e93', 'b00b1291-1da2-4807-bbe1-381c0ef61322'),
+('ef0c50d2-5846-4ffe-254a-16aff0243e77', '19f99865-defa-4161-9389-7ef231bcd626'),
+('ac7c50d0-8547-4ffe-877a-16afe5243a66', 'bfd37f57-29a5-4e40-87b2-874d228581c4');
+
+INSERT INTO referencedata.supply_lines (id, programid, supervisorynodeid, supplyingfacilityid) VALUES (
+'c8ff8b0a-d1e5-44ba-9431-70056e9cc82b', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', '6cf75e80-6405-458c-b49f-001e81411c94', 'c3c0263f-b671-4e5e-999d-f02ed290cd7b'
+),
+('26ca3e11-44b1-40d7-92f2-e188bc977550', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', '9cf76e20-6405-458c-b49f-001e81411c74', 'b00b1291-1da2-4807-bbe1-381c0ef61322'),
+('7094db14-9267-4c66-8ff8-353c3d810d63', '612fb7b7-e5bf-4de6-8f9b-313961dea46e', '7df72e02-7775-778c-b49f-001e81400d77', '76bfd0ec-55c2-4d1b-87c9-97e978b3c6b5');
